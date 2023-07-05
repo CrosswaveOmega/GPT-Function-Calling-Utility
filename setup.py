3555be09c60927dd74c28f6c939f5a0a01678ccf
CrosswaveOmega <xtream2pro@gmail.com>
2023-07-05 14:45:21 -0400

    Test of package publish
    testing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='xtream2pro@gmail.com',
     description='A simple package for the purpose of providing a set of utilities that make it easier to invoke python methods and discord.py commands with the OpenAI Function Calling API',
     keywords='discord.py, OpenAI, Function Calling API',
-    url='',
+    url='https://github.com/CrosswaveOmega/GPT-Function-Calling-Utility',
     packages=find_packages(exclude=['tests', 'docs']),
     python_requires='>=3.10',
     install_requires=[
--- src/gptfunctionlib/functionlib.py
+++ src/gptfunctionlib/functionlib.py
@@ -1,10 +1,3 @@
-#   ---------------------------------------------------------------------------------
-#   Copyright (c) Microsoft Corporation. All rights reserved.
-#   Licensed under the MIT License. See LICENSE in project root for information.
-#   ---------------------------------------------------------------------------------
-"""This is a Sample Python file."""
-
-
 import inspect
 import json
 import re
